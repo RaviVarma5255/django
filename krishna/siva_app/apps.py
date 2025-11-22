a0840b3ce95219dc56b4856f154e60ad851e8ad4
D S S RAVI VARMA <ravivarma52550@gmail.com>
2025-11-22 12:58:18 +0530

    start the app

---
+++ krishna/siva_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SivaAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'siva_app'
---
+++ krishna/siva_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+# indulo manamu functions rastaamu 
+
+def basic(request):
+
+    return HttpResponse("hello world!")
